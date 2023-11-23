1a47a61440378a91644f30ceebe26ef0d0d66f93
Bezliki7 <seki4calm@mail.ru>
2023-11-23 11:12:37 +0500

    added file assets, svg moved there

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,43 +1,30 @@
-import React from "react"
+import React from 'react';
+import ChangeSvg from '../../assets/changeSvg';
+import TrashSvg from '../../assets/trashSvg';
 
-function Contact({obj, setPopup, setCurrContact}) {
-    const sliceNumber = (n:string) =>n[0] +''+(n[0] == '8' ? '' : n[1]) +' '
-    +n.slice(-10, -7) +'-' +n.slice(-7, -4) +'-' +n.slice(-4);
-    return (
-        <>
-             <div className="contact">
-            <div> name: {obj.name} </div>
-            <div>phone number: {sliceNumber(obj.number)}</div>
-          </div>
-          <svg
-            onClick={() => {
-              setPopup(3);
-              setCurrContact(obj);
-            }}
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-pencil"
-            viewBox="0 0 16 16">
-            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
-          </svg>
-          <svg
-            onClick={() => {
-              setPopup(2);
-              setCurrContact(obj);
-            }}
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-trash"
-            viewBox="0 0 16 16">
-            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
-            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
-          </svg>
-        </>
-    )
+function Contact({ obj, setPopup, setCurrContact }) {
+  const onClickSvg = (popupType) => {
+    setPopup(popupType);
+    setCurrContact(obj);
+  };
+
+  const sliceNumber = (n: string) =>n[0] +'' +(n[0] == '8' ? '' : n[1]) +' ' +
+  n.slice(-10, -7) +'-' +n.slice(-7, -4) +'-' +n.slice(-4);
+  
+  return (
+    <>
+      <div className="contact">
+        <div> name: {obj.name} </div>
+        <div>phone number: {sliceNumber(obj.number)}</div>
+      </div>
+      <div onClick={() => onClickSvg(3)}>
+        <ChangeSvg />
+      </div>
+      <div onClick={() => onClickSvg(2)}>
+        <TrashSvg />
+      </div>
+    </>
+  );
 }
 
-export default Contact
+export default Contact;
